325c49cf446605ba9815e1a9342922a7ef2f64c9
pchums1011 <67908965+pchums1011@users.noreply.github.com>
2020-08-28 08:48:26 +0300

    Add files via upload

---
+++ data/Fire_hydrant_5.js
@@ -0,0 +1,16 @@
+var json_Fire_hydrant_5 = {
+"type": "FeatureCollection",
+"name": "Fire_hydrant_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.576125, 0.344906 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.5753, 0.356389 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.578365, 0.348985 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.58508, 0.36182 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.58314, 0.351433 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.592753, 0.351521 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.586204, 0.356413 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.587491, 0.357023 ] } },
+{ "type": "Feature", "properties": { "Name": "FH" }, "geometry": { "type": "Point", "coordinates": [ 37.594065782523259, 0.364741068623229 ] } }
+]
+}
